b97efe76c14020f4ba529c3da9162f0562a99fb8
binq-prog <hru72.twink@yandex.ru>
2026-04-06 00:26:23 +0300

    Update 1,17.c

--- 1,17.c
+++ 1,17.c
@@ -1,9 +1,31 @@
 //Косолапов 306 ис
 
 #include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char s[], int lim)
+{
+	int c, i;
+	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
 
 int main()
 {
-	printf("Значение EOF: %d\n", EOF);
+	int len;
+	char line[MAXLINE];
+	
+	while ((len = getline(line, MAXLINE)) > 0) {
+		if (len > 80) {
+			printf("%s", line);
+		}
+	}
+	
 	return 0;
 }
